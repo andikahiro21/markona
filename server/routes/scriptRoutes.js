94e6cb444a124defb15a385759040d9602e06fcd
andikahiro21 <andika.pratama@phincon.com>
2024-01-19 11:34:11 +0700

    changing method delete to post

--- server/routes/scriptRoutes.js
+++ server/routes/scriptRoutes.js
@@ -14,6 +14,6 @@ router.get('/', getScripts);
 router.get('/:id', getScriptbyID);
 router.post('/create', upload.single('file'), createScript);
 router.put('/:id', upload.single('file'), editScript);
-router.delete('/delete', deleteScripts);
+router.post('/delete', deleteScripts);
 
 module.exports = router;
